a6242dd5255b618696872ab0dda0e29c534fc5a2
liaol <yicoliao@gmail.com>
2016-02-24 15:12:44 +0800

    ipip.net 需要设置useragent

--- IPIP.popclipext/main.php
+++ IPIP.popclipext/main.php
@@ -1,8 +1,19 @@
 <?php 
+
 $input=trim(getenv('POPCLIP_TEXT'));
 
 $api = 'http://freeapi.ipip.net/';
-$data = file_get_contents($api . $input);
+$opts = array(
+    'http'=>array(
+        'method'=>"GET",
+        'header'=>"User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36\r\n",
+    )
+);
+
+$input = '114.114.114.114';
+$context = stream_context_create($opts);
+$url = $api . $input;
+$data = file_get_contents($url, false, $context);
 
 if (!empty($data)) {
     $data = json_decode($data, true);
@@ -15,3 +26,4 @@ if (!empty($data)) {
 } else {
     echo 'fail';
 }
+
